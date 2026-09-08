7872cb0c857485d911547365a645d8731b125038
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2026-09-08 12:57:58 +0200

    finished admin wrapper component

--- src/components/OnlyAdmin.jsx
+++ src/components/OnlyAdmin.jsx
@@ -1,8 +1,18 @@
 // adding the functionaly for protecting pages only for admins. WIP.
 
-function OnlyAdmin() {
-  return (
-    <div>OnlyAdmin</div>
-  )
+import { useContext } from "react"
+import { AuthContext } from "../context/auth.context"
+import { Navigate } from "react-router-dom"
+
+function OnlyAdmin(props) {
+
+  const { isLoggedIn, loggedUserRole } = useContext(AuthContext)
+
+  if (isLoggedIn && loggedUserRole === "admin") {
+    return props.children // you can see an admin page
+  } else {
+    return <Navigate to="/login"/>
+  }
+
 }
 export default OnlyAdmin
